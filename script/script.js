fc58112667497214c3e05f0bba192689541d44ac
Yreish Pozo <101380787+zo-world@users.noreply.github.com>
2023-02-28 18:22:20 -0800

    Update
    Class 2 progress

---
+++ script/script.js
@@ -0,0 +1,22 @@
+console.log("Script");
+
+let latestStry = "Latest Story";
+let stry1Title = "[Science] Mind, Body, Wonder";
+let stry1Desc = "How to take better care of your aging brain"; 
+let stry2Title = "Animals";
+let stry2Desc = "How did this ountain lion reach an uninhabited island?";
+let stry3Title = "Enviroment";
+let stry3Desc = "Rest in ... compost? Eco-friendly burial alternatives, explained";
+let stry4Title = "Science"
+let stry4Desc = "This diseases often goes under-diagnosed-unless you're white";
+
+
+document.write("<h1>"+ latestStry +"</h1>");
+document.write("<h2>"+ stry1Title + "</h2>");
+document.write("<p>"+ stry1Desc + "</p>");
+document.write("<h2>"+ stry2Title + "</h2>");
+document.write("<p>"+ stry2Desc + "</p>");
+document.write("<h2>"+ stry3Title + "</h2>");
+document.write("<p>"+ stry3Desc + "</p>");
+document.write("<h2>"+ stry4Title + "</h2>");
+document.write("<p>"+ stry4Desc + "</p>");
