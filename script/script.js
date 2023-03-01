709f96d5198735d7400f44aed2894e6d37756b34
Yreish Pozo <101380787+zo-world@users.noreply.github.com>
2023-02-28 18:43:31 -0800

    Update
    Class 2

--- script/script.js
+++ script/script.js
@@ -9,6 +9,10 @@ let stry3Title = "Enviroment";
 let stry3Desc = "Rest in ... compost? Eco-friendly burial alternatives, explained";
 let stry4Title = "Science"
 let stry4Desc = "This diseases often goes under-diagnosed-unless you're white";
+let stry5Title = "Mind, Body, Wonder";
+let stry5Desc = "The groundbreaking promise of 'cellular housekeeping'";
+let stry6Title = "Animals";
+let stry6Desc = "What does cancer smell like? These animals cam sniff it out";
 
 
 document.write("<h1>"+ latestStry +"</h1>");
@@ -20,3 +24,7 @@ document.write("<h2>"+ stry3Title + "</h2>");
 document.write("<p>"+ stry3Desc + "</p>");
 document.write("<h2>"+ stry4Title + "</h2>");
 document.write("<p>"+ stry4Desc + "</p>");
+document.write("<h2>"+ stry5Title + "</h2>");
+document.write("<p>"+ stry5Desc + "</p>");
+document.write("<h2>"+ stry6Title + "</h2>");
+document.write("<p>"+ stry6Desc + "</p>");
